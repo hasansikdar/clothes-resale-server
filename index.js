24bdbe13a518b102784f3d5e67e77e16e76bde49
hasansikdar <hasansikdar973@gmail.com>
2022-11-24 23:06:36 +0600

    authentication added

--- index.js
+++ index.js
@@ -10,17 +10,47 @@ app.use(cors())
 app.use(express.json())
 
 
-async function run(){
-    try{
+async function run() {
+    try {
         const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0.i9b8vs8.mongodb.net/?retryWrites=true&w=majority`;
         const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
         const usersCollection = client.db('resaleProduct').collection('users');
+        const productsCollection = client.db('resaleProduct').collection('products');
 
-        
-        
-    
+        app.post('/users', async(req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            res.send(result);
+        })
+
+
+        app.post('/addProduct', async (req, res) => {
+            const product = req.body;
+            const result = await productsCollection.insertOne(product);
+            res.send(result);
+        })
+
+        // men, women, baby category loaded
+        app.get('/products', async (req, res) => {
+            const query = {
+                category: 'gender',
+            }
+
+            const result = await productsCollection.find(query).toArray();
+            res.send(result);
+
+        })
+        app.get('/productCategory/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {
+                productCategory: id
+            }
+            const result = await productsCollection.find(query).toArray();
+            res.send(result);
+        })
+
     }
-    finally{
+    finally {
 
     }
 }
